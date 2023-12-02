77a2afd5eb85a7b0ca3ae4e808630ff161dd68e0
Alexander Erlemann <alexander.erlemann@tiusoft.com>
2023-12-02 19:02:13 +0100

    Day 2 Part 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -27,6 +27,22 @@ class Day2 {
         println(possibleGameIds.sum())
     }
 
+    fun solvePart2() {
+        val inputs = this::class.java.getResource("day2_1.txt")?.let { readFile(it.toURI()) }
+
+        val powersOfGames = ArrayList<Int>()
+
+        inputs?.forEach { line ->
+            val sets = line.substring(line.indexOf(':') + 1).split(';')
+
+            val maxValuesInGame = getMaxValuesInGame(sets)
+
+            powersOfGames.add(maxValuesInGame.maxNumberBlue * maxValuesInGame.maxNumberRed * maxValuesInGame.maxNumberGreen)
+        }
+
+        println(powersOfGames.sum())
+    }
+
     private fun isSetPossible(set: String): Boolean {
         val colorsWithNumbers = set.split(',')
 
@@ -52,5 +68,42 @@ class Day2 {
         return true
     }
 
+    private fun getMaxValuesInGame(sets: List<String>): MaxValuesInGame {
+        var maxNumberBlue = 0
+        var maxNumberRed = 0
+        var maxNumberGreen = 0
+
+        for (set in sets) {
+            val colorsWithNumbers = set.split(',')
+
+            for (color in colorsWithNumbers) {
+                val colorCubeNumber = color.filter { it.isDigit() }.toInt()
+                if (color.contains("red")) {
+                    if (colorCubeNumber > maxNumberRed) {
+                        maxNumberRed = colorCubeNumber
+                    }
+                }
+                if (color.contains("green")) {
+                    if (colorCubeNumber > maxNumberGreen) {
+                        maxNumberGreen = colorCubeNumber
+                    }
+                }
+                if (color.contains("blue")) {
+                    if (colorCubeNumber > maxNumberBlue) {
+                        maxNumberBlue = colorCubeNumber
+                    }
+                }
+            }
+        }
+
+        return MaxValuesInGame(maxNumberRed, maxNumberGreen, maxNumberBlue)
+    }
+
     private fun readFile(fileName: URI): List<String> = File(fileName).readLines()
+
+    data class MaxValuesInGame(
+        val maxNumberRed: Int,
+        val maxNumberGreen: Int,
+        val maxNumberBlue: Int
+    )
 }
